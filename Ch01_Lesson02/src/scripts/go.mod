1e8f0da1a5204c73b67b08775e78f8468a40530b
Nasztanovics Dávid <david.nasztanovics@stylersonline.com>
2020-10-26 17:39:20 +0100

    create migration

---
+++ Ch01_Lesson02/src/scripts/go.mod
@@ -0,0 +1,5 @@
+module github.com/dNaszta/ms_go/Ch01_Lesson02/src/scripts
+
+go 1.13
+
+require go.mongodb.org/mongo-driver v1.4.2 // indirect
--- Ch01_Lesson02/src/scripts/main.go
+++ Ch01_Lesson02/src/scripts/main.go
@@ -1,26 +1,75 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"io/ioutil"
-	"log"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"io/ioutil"
+	"log"
 )
 
 type timeZoneConversion struct {
-	TimeZone		string `bson:"timeZone" json:"timeZone"`
-	TimeDifference	string `bson:"timeDifference" json:"timeDifference"`
-	Name			string `bson:"name" json:"name"`
+	TimeZone		string `json:"timeZone"`
+	TimeDifference	string `json:"timeDifference"`
+	Name			string `json:"name"`
 }
 
 type tzs []timeZoneConversion
 
 func main() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	const database = "packt"
+	const username = "root"
+	const password = "example"
+	const collectionName = "timeZones"
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017").SetAuth(
+		options.Credential{
+			Username: username,
+			Password: password,
+		})
+
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatal("Couldn't connect to MongoDB")
 	}
+	defer client.Disconnect(context.TODO())
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Connected to MongoDB!")
+
+	collection := client.Database(database).Collection(collectionName)
+	err = collection.Drop(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf(`"%s" database deleted`, database)
+
+	startDatabase := client.Database(database)
+	timezoneCollection := startDatabase.Collection(collectionName)
+	log.Printf(`"%s" database "%s" collection created`, database, collectionName)
+
+	data, err := ioutil.ReadFile("all_timezones.json")
+	if err != nil {
+		log.Fatal("Couldn't open file")
+	}
+
+	var timeZones tzs
+	err = json.Unmarshal(data, &timeZones)
+	if err != nil {
+		log.Fatal("Couldn't unmarshall Json")
+	}
+
+	i := 0
+	for _, v := range timeZones {
+		timezoneCollection.InsertOne(context.TODO(), v)
+		i++
+	}
+	log.Printf("%d record inserted", i)
 }
